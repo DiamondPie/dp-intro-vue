4cccda8ebe1825e01bcb7131a6faedd610e069b7
DiamondPie <thelostdiamondpie@outlook.com>
2026-06-10 02:00:54 +1200

    fix: language icon will switch by marking it ClientOnly

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -40,41 +40,43 @@
                     filter: drop-shadow(rgba(233, 160, 255, 0.5) 0px 0px 20px);
                   "
                 >
-                  <!-- English icon — shown when current locale is en -->
-                  <svg
-                    v-if="locale === 'en'"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    aria-hidden="true"
-                    role="img"
-                    class="iconify iconify--simple-icons"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 48 48"
-                  >
-                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="4">
-                      <rect width="36" height="36" x="6" y="6" stroke-linejoin="round" rx="3" />
-                      <path stroke-linejoin="round" d="M14 18h20v10H14z" />
-                      <path d="M24 14v21" />
-                    </g>
-                  </svg>
-                  <!-- Chinese icon — shown when current locale is zh -->
-                  <svg
-                    v-else
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    aria-hidden="true"
-                    role="img"
-                    class="iconify iconify--simple-icons"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 48 48"
-                  >
-                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
-                      <rect width="36" height="36" x="6" y="6" rx="3" />
-                      <path d="M13 31V17h8m-8 7h7.5M13 31h7.5m5.5 0V19m0 12v-6.5a4.5 4.5 0 0 1 4.5-4.5v0a4.5 4.5 0 0 1 4.5 4.5V31" />
-                    </g>
-                  </svg>
+                  <ClientOnly>
+                    <!-- English icon — shown when current locale is en -->
+                    <svg
+                      v-if="locale === 'en'"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      aria-hidden="true"
+                      role="img"
+                      class="iconify iconify--simple-icons"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 48 48"
+                    >
+                      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="4">
+                        <rect width="36" height="36" x="6" y="6" stroke-linejoin="round" rx="3" />
+                        <path stroke-linejoin="round" d="M14 18h20v10H14z" />
+                        <path d="M24 14v21" />
+                      </g>
+                    </svg>
+                    <!-- Chinese icon — shown when current locale is zh -->
+                    <svg
+                      v-else
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      aria-hidden="true"
+                      role="img"
+                      class="iconify iconify--simple-icons"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 48 48"
+                    >
+                      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+                        <rect width="36" height="36" x="6" y="6" rx="3" />
+                        <path d="M13 31V17h8m-8 7h7.5M13 31h7.5m5.5 0V19m0 12v-6.5a4.5 4.5 0 0 1 4.5-4.5v0a4.5 4.5 0 0 1 4.5 4.5V31" />
+                      </g>
+                    </svg>
+                  </ClientOnly>
                 </div>
               </div>
             </a>
