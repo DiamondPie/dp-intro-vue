8126d8ea6b09f8ccaf53f0ed704a2a69139540a5
DiamondPie <thelostdiamondpie@outlook.com>
2026-06-16 00:08:40 +1200

    fix: normalize asset path

--- functions/exam-clock/[[path]].js
+++ functions/exam-clock/[[path]].js
@@ -1,11 +1,13 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  
+
   if (url.pathname === '/exam-clock') {
     return Response.redirect(url.origin + '/exam-clock/', 301);
   }
-  
-  const targetUrl = 'https://dp-exam-clock.pages.dev' + url.pathname + url.search;
+
+  const targetPath = url.pathname.replace(/^\/exam-clock/, '');
+
+  const targetUrl = 'https://dp-exam-clock.pages.dev' + targetPath + url.search;
 
   const response = await fetch(targetUrl, {
     method: context.request.method,
