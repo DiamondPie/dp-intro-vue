3055879f32624dd94be312e7cdcf62d4f03d93be
DiamondPie <thelostdiamondpie@outlook.com>
2026-06-15 23:53:17 +1200

    fix: normalize request path

--- functions/exam-clock/[[path]].js
+++ functions/exam-clock/[[path]].js
@@ -1,6 +1,10 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   
+  if (url.pathname === '/blog') {
+    return Response.redirect(url.origin + '/blog/', 301);
+  }
+  
   const targetUrl = 'https://dp-exam-clock.pages.dev' + url.pathname + url.search;
 
   const response = await fetch(targetUrl, {
