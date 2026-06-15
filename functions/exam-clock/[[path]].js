1b173d7213edf444a91a23f3c8c4ba73264eea16
DiamondPie <thelostdiamondpie@outlook.com>
2026-06-15 23:55:29 +1200

    typo: correct `/blog` to `/exam-clock`

--- functions/exam-clock/[[path]].js
+++ functions/exam-clock/[[path]].js
@@ -1,8 +1,8 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   
-  if (url.pathname === '/blog') {
-    return Response.redirect(url.origin + '/blog/', 301);
+  if (url.pathname === '/exam-clock') {
+    return Response.redirect(url.origin + '/exam-clock/', 301);
   }
   
   const targetUrl = 'https://dp-exam-clock.pages.dev' + url.pathname + url.search;
